ecd8fc4f7b20ab53166ee8c0551bde4366e57a00
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-04 11:33:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!bin/bash
 import time
 
+echo "Configurating interfaces..."
 simple_switch -i 0@s1-eth0 -i 1@s1-eth1 --nanolog ipc:///tmp/bm-log.ipc basic.json &
-time.sleep(3)
+echo "Complete!"
+
+echo "Populating tables with forwarding rules..."
+sleep 3
 simple_switch_CLI < ~/lab2/rules.cmd
+echo "Complete!"
