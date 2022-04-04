ef1c5834c134a10935a5ef1207b4ec7a9496c2b0
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-04 11:36:28 -0400

    Create register_read.cmd

---
+++ register_read.cmd
@@ -0,0 +1,9 @@
+register_read r_srcAddr 0
+register_read r_dstAddr 0
+register_read r_startTime 0
+register_read r_endTime 0
+register_read r_totalSize 0
+register_read r_srcPort 0
+register_read r_dstPort 0
+register_read r_duration 0
+
