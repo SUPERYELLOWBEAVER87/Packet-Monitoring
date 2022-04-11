2ad8df7f0eabb7fa014a0283ca9b7e28c6a46034
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-11 09:05:45 -0400

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+if [ -f index.cmd ]
+then
+    rm index.cmd
+fi
+echo -e "Enter register index:\n"
+read index
+echo "register_read r_srcAddr $index" >> index.cmd
+echo "register_read r_dstAddr $index" >> index.cmd
+echo "register_read r_startTime $index" >> index.cmd
+echo "register_read r_endTime $index" >> index.cmd
+echo "register_read r_totalSize $index" >> index.cmd
+echo "register_read r_srcPort $index" >> index.cmd
+echo "register_read r_dstPort $index" >> index.cmd
+echo "register_read r_duration $index" >> index.cmd
