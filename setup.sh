cb1625a437364f65f54bb6cf015559c7eb9d8473
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-11 09:11:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 echo -e "\nDownloading P4 configuration file...\n"
 wget https://raw.githubusercontent.com/SUPERYELLOWBEAVER87/P4-Packet-Monitoring/main/basic.json
-sleep 3
+sleep 2
 
 echo -e "\nConfigurating interfaces...\n"
 simple_switch -i 0@s1-eth0 -i 1@s1-eth1 --nanolog ipc:///tmp/bm-log.ipc basic.json &
 
-sleep 3
+sleep 2
 
 echo -e "\nPopulating tables with forwarding rules...\n"
 simple_switch_CLI < ~/lab2/rules.cmd
-sleep 3
+sleep 2
 echo -e "\nComplete!\n"
 
 echo -e "\nDownloading additional files...\n"
