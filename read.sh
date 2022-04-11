68576140481b907cb952482f754aff919e20afcb
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-11 09:18:24 -0400

    Update read.sh

--- read.sh
+++ read.sh
@@ -1,8 +1,26 @@
+#!bin/bash
+if [ -f index.cmd ]
+then
+    rm index.cmd
+fi
+if [ -z $1 ]
+then
+    echo -e "Enter register index:\n"
+    read index
+fi
+echo "register_read r_srcAddr $index" >> index.cmd
+echo "register_read r_dstAddr $index" >> index.cmd
+echo "register_read r_startTime $index" >> index.cmd
+echo "register_read r_endTime $index" >> index.cmd
+echo "register_read r_totalSize $index" >> index.cmd
+echo "register_read r_srcPort $index" >> index.cmd
+echo "register_read r_dstPort $index" >> index.cmd
+echo "register_read r_duration $index" >> index.cmd
 if [ -f register_value ]
 then
   rm register_value
 fi
 simple_switch_CLI < index.cmd
-sleep 2
+sleep 1
 python register_formatter.py
 
