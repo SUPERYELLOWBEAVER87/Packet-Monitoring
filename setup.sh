75c9da1324bea7f6842d33e4a1edf5b06b43f396
SUPERYELLOWBEAVER87 <78384615+SUPERYELLOWBEAVER87@users.noreply.github.com>
2022-04-11 09:11:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!bin/bash
 echo -e "\nDownloading P4 configuration file...\n"
 wget https://raw.githubusercontent.com/SUPERYELLOWBEAVER87/P4-Packet-Monitoring/main/basic.json
-sleep 5
+sleep 3
 
 echo -e "\nConfigurating interfaces...\n"
 simple_switch -i 0@s1-eth0 -i 1@s1-eth1 --nanolog ipc:///tmp/bm-log.ipc basic.json &
 
-sleep 5
+sleep 3
 
 echo -e "\nPopulating tables with forwarding rules...\n"
 simple_switch_CLI < ~/lab2/rules.cmd
-sleep 5
+sleep 3
 echo -e "\nComplete!\n"
 
 echo -e "\nDownloading additional files...\n"
@@ -24,7 +24,7 @@ echo -e "\nReplacing current runtime_CLI.py with modified runtime...\n"
 sleep 2
 
 wget https://raw.githubusercontent.com/SUPERYELLOWBEAVER87/P4-Packet-Monitoring/main/runtime_CLI.py
-sleep 3
+sleep 2
 cp -f runtime_CLI.py /usr/local/lib/python3.5/site-packages/runtime_CLI.py
 
 echo "Cleaning up..."
